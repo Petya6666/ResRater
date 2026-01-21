ea89e2fd521b9d548062b092bb76851fdb335d21
palpet478 <palpet478@hengersor.hu>
2026-01-21 11:29:14 +0100

    profil kartyk

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,50 @@
 import React from 'react'
 import Header from '../components/Header.jsx'
 import '../styles/index.css';
+import Card from 'react-bootstrap/Card';
 
 function Home(){
     return (
     <>
       <Header />
-        <br />
+      <br />
 
-        <div className='doboz'>
-        <h1>Üdvözöljük a ResRater alkalmazásban!</h1>
-        <br />
-        <p>Fedezze fel a legjobb éttermeket, olvassa el a véleményeket, és ossza meg saját tapasztalatait másokkal!</p> 
-        </div>
+      <div className='doboz'>
+      <h1>Üdvözöljük a ResRater alkalmazásban!</h1>
+      <br />
+      <p>Fedezze fel a legjobb éttermeket, olvassa el a véleményeket, és ossza meg saját tapasztalatait másokkal!</p> 
+      <br />
+      <br />
+      </div>
+      <div className='card-container' >
+      <Card >
+      <Card.Img variant="top" src="./public/noFilter.png"  className='kep'/>
+      <Card.Body className='kartya'>
+        <Card.Title className='feher'>Kardos Tamás</Card.Title>
+        <Card.Text className='feher'>
+        Backend fejlesztő
+        </Card.Text>
+      </Card.Body>
+      </Card>
+      <Card >
+      <Card.Img variant="top" src="./public/noFilter.png"  className='kep'/>
+      <Card.Body className='kartya'>
+        <Card.Title className='feher'>Pálházi Péter</Card.Title>
+        <Card.Text className='feher'>
+        Frontend fejlesztő
+        </Card.Text>
+      </Card.Body>
+      </Card>
+      <Card >
+      <Card.Img variant="top" src="./public/noFilter.png"  className='kep' />
+      <Card.Body className='kartya'>
+        <Card.Title className='feher'>Papp Zsombor</Card.Title>
+        <Card.Text className='feher'>
+        Adatbázis fejlesztő
+        </Card.Text>
+      </Card.Body>
+      </Card>
+      </div>
     </>
     )
 }
