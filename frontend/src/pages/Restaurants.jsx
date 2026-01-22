605e903441296447b2858903ad51b7a58e2b2ec6
palpet478 <palpet478@hengersor.hu>
2026-01-22 12:56:28 +0100

    kezdetleges etterem kepek

--- frontend/src/pages/Restaurants.jsx
+++ frontend/src/pages/Restaurants.jsx
@@ -28,8 +28,10 @@ const Restaurants = () => {
             <div key={etterem.nev} className='col-md-4 mb-4'>
               <div className='card'>
                 <div className='card-body'>
+                  <img className='card-img-top' src={etterem.url} alt={etterem.nev}  />
                   <h5 className='card-title'>{etterem.nev}</h5>
                   <p className='card-text'>Város: {etterem.varos}</p>
+                  <p className='card-text'>Átlagos értékelés: {etterem.atlag}</p>
                 </div>
               </div>
             </div>
