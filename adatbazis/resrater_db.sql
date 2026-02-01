9f68677a711a458c5c8aeacf80a3dbfeca01c684
Zsombor656 <papzso656@hengersor.hu>
2026-02-01 17:08:28 +0100

    Update resrater_db.sql

--- adatbazis/resrater_db.sql
+++ adatbazis/resrater_db.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Gép: 127.0.0.1:3307
--- Létrehozás ideje: 2026. Jan 22. 12:48
--- Kiszolgáló verziója: 10.4.28-MariaDB
--- PHP verzió: 8.2.4
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2026. Feb 01. 17:07
+-- Kiszolgáló verziója: 10.4.32-MariaDB
+-- PHP verzió: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Adatbázis: `resrater_db`
 --
-CREATE DATABASE IF NOT EXISTS `resrater_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `resrater_db`;
 
 -- --------------------------------------------------------
 
@@ -66,28 +64,28 @@ INSERT INTO `ertekelesek` (`ertekeles_id`, `etterem_id`, `felhasznalo_id`, `atla
 CREATE TABLE `ettermek` (
   `etterem_id` int(11) NOT NULL,
   `nev` varchar(100) NOT NULL,
-  `varos` varchar(100) NOT NULL,
-  `iranyitoszam` varchar(10) NOT NULL,
   `telefon` varchar(20) NOT NULL,
   `leiras` text NOT NULL,
-  `kategoria` varchar(50) NOT NULL
+  `kategoria` varchar(50) NOT NULL,
+  `varosok_id` int(11) DEFAULT NULL,
+  `jovahagyott` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
 -- A tábla adatainak kiíratása `ettermek`
 --
 
-INSERT INTO `ettermek` (`etterem_id`, `nev`, `varos`, `iranyitoszam`, `telefon`, `leiras`, `kategoria`) VALUES
-(1, 'BurgerPart', 'Budapest', '1188', '+365148769', 'Kézműves burgerek, ropogós krumpli és jó hangulat – ez a BurgerPart receptje. Tökéletes hely egy gyors falatra a barátokkal vagy egy laza ebédszünetre.', '2'),
-(2, 'Legjobb csárda', 'Inárcs', '2365', '+3642069', 'A Legjobb Csárda a hagyományos magyar vendégszeretet otthona, ahol az igazi falusi ízek és a jókedv kéz a kézben járnak. Frissen készült, házias ételeink – a gőzölgő gulyástól a ropogós rántott húsig – a nagymama konyhájának hangulatát idézik. A barátságos kiszolgálás, a rusztikus berendezés és a vidám zene gondoskodik róla, hogy minden vendég úgy érezze: itt tényleg otthon van.', '1'),
-(3, 'Deák Ferenc Falatozó', 'Budapest', '1044', '+3655877', 'A Deák Ferenc Falatozó egy hangulatos, családias étterem, ahol a hagyományos magyar ízek találkoznak a modern konyha könnyedségével. Éttermünk szívében a vendég a legfontosabb: friss, helyi alapanyagokból készült ételeinket barátságos kiszolgálással és otthonos környezetben kínáljuk. Legyen szó egy gyors ebédről, baráti vacsoráról vagy hétvégi lakomáról, nálunk mindig jóllakottan és mosollyal az arcán távozik minden vendég.', '2'),
-(4, 'Kacsa a Ködben', 'Budapest', '1073', '+36598742', 'A Kacsa a Ködben egy hangulatos, kissé titokzatos étterem, ahol a gasztronómia és a művészet találkozik. Rejtett utcában, lágy fények és kellemes zene kíséretében várjuk vendégeinket, hogy felfedezzék a magyar és nemzetközi konyha különleges fúzióját. Éttermünk védjegye a kreatív kacsás fogások sora – a klasszikus sült kacsától a modern, gyümölcsös variációkig.', '2'),
-(5, 'Lángoszóna', 'Budapest', '1075', '+36548798', 'A Lángoszóna a magyar street food lelke. Klasszikus és felturbózott lángosok, fokhagymás illat és friss tejföl – hagyomány, ami belefér a kezedbe.', '1'),
-(6, 'Piros Paprika Vendéglő', 'Debrecen', '4025', '+36985678', 'A Piros Paprika Vendéglő a magyar ízek színes otthona, ahol minden falatban ott van a tradíció és a szenvedély. Friss, hazai alapanyagokból készült ételeinket gazdagon fűszerezzük – természetesen jó adag szeretettel és egy csipetnyi paprikával. Barátságos hangulat, házias ízek és bőséges adagok várják mindazokat, akik szeretik a klasszikus magyar konyhát.', '2'),
-(7, 'Aranykanál Bisztró', 'Győr', '9022', '+3696554432', 'Az Aranykanál Bisztró elegáns, mégis otthonos hely, ahol a modern magyar konyha kifinomult ízei várják a vendégeket.', '2'),
-(8, 'Bors & Lélek', 'Szeged', '6725', '+3662314875', 'A Bors & Lélek modern hangulatú étterem, ahol a bor és a fűszer találkozik – magyaros ételek újragondolva.', '1'),
-(9, 'Hot & Go', 'Budapest', '1138', '+3652478215', 'A Hot & Go fűszeres gyorsételeket kínál lendületes, fiatalos környezetben – tökéletes útközben is.', '1'),
-(10, 'ZabálÓra', 'Pécs', '1195', '+3672611998', 'A ZabálÓra a rohanó vendégek kedvence: friss, bőséges és gyors ételek barátságos hangulatban.', '2');
+INSERT INTO `ettermek` (`etterem_id`, `nev`, `telefon`, `leiras`, `kategoria`, `varosok_id`, `jovahagyott`) VALUES
+(1, 'BurgerPart', '+365148769', 'Kézműves burgerek, ropogós krumpli és jó hangulat – ez a BurgerPart receptje. Tökéletes hely egy gyors falatra a barátokkal vagy egy laza ebédszünetre.', '2', 1, 1),
+(2, 'Legjobb csárda', '+3642069', 'A Legjobb Csárda a hagyományos magyar vendégszeretet otthona, ahol az igazi falusi ízek és a jókedv kéz a kézben járnak. Frissen készült, házias ételeink – a gőzölgő gulyástól a ropogós rántott húsig – a nagymama konyhájának hangulatát idézik. A barátságos kiszolgálás, a rusztikus berendezés és a vidám zene gondoskodik róla, hogy minden vendég úgy érezze: itt tényleg otthon van.', '1', 3, 1),
+(3, 'Deák Ferenc Falatozó', '+3655877', 'A Deák Ferenc Falatozó egy hangulatos, családias étterem, ahol a hagyományos magyar ízek találkoznak a modern konyha könnyedségével. Éttermünk szívében a vendég a legfontosabb: friss, helyi alapanyagokból készült ételeinket barátságos kiszolgálással és otthonos környezetben kínáljuk. Legyen szó egy gyors ebédről, baráti vacsoráról vagy hétvégi lakomáról, nálunk mindig jóllakottan és mosollyal az arcán távozik minden vendég.', '2', 1, 0),
+(4, 'Kacsa a Ködben', '+36598742', 'A Kacsa a Ködben egy hangulatos, kissé titokzatos étterem, ahol a gasztronómia és a művészet találkozik. Rejtett utcában, lágy fények és kellemes zene kíséretében várjuk vendégeinket, hogy felfedezzék a magyar és nemzetközi konyha különleges fúzióját. Éttermünk védjegye a kreatív kacsás fogások sora – a klasszikus sült kacsától a modern, gyümölcsös variációkig.', '2', 2, 1),
+(5, 'Lángoszóna', '+36548798', 'A Lángoszóna a magyar street food lelke. Klasszikus és felturbózott lángosok, fokhagymás illat és friss tejföl – hagyomány, ami belefér a kezedbe.', '1', 3, 0),
+(6, 'Piros Paprika Vendéglő', '+36985678', 'A Piros Paprika Vendéglő a magyar ízek színes otthona, ahol minden falatban ott van a tradíció és a szenvedély. Friss, hazai alapanyagokból készült ételeinket gazdagon fűszerezzük – természetesen jó adag szeretettel és egy csipetnyi paprikával. Barátságos hangulat, házias ízek és bőséges adagok várják mindazokat, akik szeretik a klasszikus magyar konyhát.', '2', 1, 0),
+(7, 'Aranykanál Bisztró', '+3696554432', 'Az Aranykanál Bisztró elegáns, mégis otthonos hely, ahol a modern magyar konyha kifinomult ízei várják a vendégeket.', '2', 2, 1),
+(8, 'Bors & Lélek', '+3662314875', 'A Bors & Lélek modern hangulatú étterem, ahol a bor és a fűszer találkozik – magyaros ételek újragondolva.', '1', 3, 1),
+(9, 'Hot & Go', '+3652478215', 'A Hot & Go fűszeres gyorsételeket kínál lendületes, fiatalos környezetben – tökéletes útközben is.', '1', 1, 0),
+(10, 'ZabálÓra', '+3672611998', 'A ZabálÓra a rohanó vendégek kedvence: friss, bőséges és gyors ételek barátságos hangulatban.', '2', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -143,7 +141,37 @@ CREATE TABLE `kepek` (
 --
 
 INSERT INTO `kepek` (`kep_id`, `etterem_id`, `url`, `leiras`, `feltoltes_datum`) VALUES
-(1, 10, 'https://tse3.mm.bing.net/th/id/OIP.S0Ih7cACnm3f0Jv5YJ2oaQHaGY?rs=1&pid=ImgDetMain&o=7&rm=3', 'ez egy kfc', '2026-01-22 12:44:39');
+(1, 9, 'https://unsplash.com/s/photos/restaurant', 'ez egy étterem', '2026-01-22 12:44:39'),
+(2, 10, 'https://unsplash.com/s/photos/restaurant', 'Belső éttermi hangulat, teríték és gyertyák', '2026-02-01 17:02:22'),
+(3, 8, 'https://stock.adobe.com/search?k=hotel+restaurant', 'Vacsora két főre – elegáns szervírozás', '2026-02-01 17:02:22'),
+(4, 7, 'https://create.vista.com/photos/fine-restaurants/', 'Fine dining tálalás közelről', '2026-02-01 17:02:22'),
+(5, 6, 'https://www.dreamstime.com/photos-images/modern-cozy-restaurant.html', 'Séf munka közben a konyhában', '2026-02-01 17:02:22'),
+(6, 5, 'https://www.freepik.com/free-photos-vectors/restaurant', 'Modern étterem enteriőr', '2026-02-01 17:02:22'),
+(7, 4, 'https://stock.adobe.com/search?k=empty+table+restaurant', 'Asztalterítés borospoharakkal', '2026-02-01 17:02:22'),
+(8, 3, 'https://www.istockphoto.com/photos/fine-dining-restaurant-interior', 'Ínyenc fogás – kreatív tálalás', '2026-02-01 17:02:22'),
+(9, 2, 'https://depositphotos.com/hu/photos/restaurant.html', 'Forró főétel felszolgálva', '2026-02-01 17:02:22'),
+(10, 1, 'https://www.vecteezy.com/free-photos/fancy-restaurant', 'Felszolgáló italokat hoz az asztalhoz', '2026-02-01 17:02:22');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `varosok`
+--
+
+CREATE TABLE `varosok` (
+  `iranyitoszam` varchar(100) NOT NULL,
+  `varos` varchar(100) NOT NULL,
+  `varosok_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- A tábla adatainak kiíratása `varosok`
+--
+
+INSERT INTO `varosok` (`iranyitoszam`, `varos`, `varosok_id`) VALUES
+('1117', 'Budapest', 1),
+('9022', 'Győr', 2),
+('6720', 'Szeged', 3);
 
 --
 -- Indexek a kiírt táblákhoz
@@ -161,7 +189,8 @@ ALTER TABLE `ertekelesek`
 -- A tábla indexei `ettermek`
 --
 ALTER TABLE `ettermek`
-  ADD PRIMARY KEY (`etterem_id`);
+  ADD PRIMARY KEY (`etterem_id`),
+  ADD KEY `varosok_id` (`varosok_id`);
 
 --
 -- A tábla indexei `felhasznalok`
@@ -175,6 +204,12 @@ ALTER TABLE `felhasznalok`
 ALTER TABLE `kepek`
   ADD PRIMARY KEY (`kep_id`),
   ADD KEY `fk_kepek_id` (`etterem_id`);
+
+--
+-- A tábla indexei `varosok`
+--
+ALTER TABLE `varosok`
+  ADD PRIMARY KEY (`varosok_id`);
 
 --
 -- A kiírt táblák AUTO_INCREMENT értéke
@@ -196,6 +231,12 @@ ALTER TABLE `felhasznalok`
 ALTER TABLE `ertekelesek`
   ADD CONSTRAINT `ertekelesek_ibfk_1` FOREIGN KEY (`etterem_id`) REFERENCES `ettermek` (`etterem_id`),
   ADD CONSTRAINT `fk_felhasznalo_id` FOREIGN KEY (`felhasznalo_id`) REFERENCES `felhasznalok` (`felhasznalo_id`);
+
+--
+-- Megkötések a táblához `ettermek`
+--
+ALTER TABLE `ettermek`
+  ADD CONSTRAINT `fk_varos` FOREIGN KEY (`varosok_id`) REFERENCES `varosok` (`varosok_id`);
 
 --
 -- Megkötések a táblához `kepek`
